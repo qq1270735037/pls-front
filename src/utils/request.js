ac7f18c0607c21926e3c921582310acd390fab4f
wlt <857582847@qq.com>
2022-06-27 14:52:23 +0800

    修改接口，使用dataresult封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,38 +11,38 @@ const service = axios.create({
 })
 
 // http request 拦截器
-service.interceptors.request.use(
-	config => {
-		return config
-	},
-	error => {
-		return Promise.reject(error)
-	}
-)
-
-// service.interceptors.response.use(
-// 	response => {
-// 		console.log(response)
-// 		if(response.data){//服务器返回了数据
-// 			if(response.data.code == 200 ){
-// 				//服务器返回了正确的数据
-// 				return response.data;
-// 			}else if(response.data.code == -2){
-// 				//登录失效, 跳转到登录页面
-// 				Router.push({name : "login",query: {message: "登录失效"}});
-// 				return false;
-// 			}else{
-// 				Message({
-// 					message: response.data.message,
-// 					type: "error"
-// 				})
-// 			}
-// 		}
-// 		return -1;
+// service.interceptors.request.use(
+// 	config => {
+// 		return config
 // 	},
 // 	error => {
 // 		return Promise.reject(error)
 // 	}
 // )
+
+service.interceptors.response.use(
+	response => {
+		console.log(response)
+		if(response.data){//服务器返回了数据
+			if(response.data.code == 200 ){
+				//服务器返回了正确的数据
+				return response.data;
+			}else if(response.data.code == -2){
+				//登录失效, 跳转到登录页面
+				Router.push({name : "login",query: {message: "登录失效"}});
+				return false;
+			}else{
+				Message({
+					message: response.data.message,
+					type: "error"
+				})
+			}
+		}
+		return -1;
+	},
+	error => {
+		return Promise.reject(error)
+	}
+)
 
 export default service
