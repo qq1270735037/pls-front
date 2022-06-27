51c78346b3b07a90c8b8d474541cae8de9d1745b
wlt <857582847@qq.com>
2022-06-27 23:43:02 +0800

    carinfo分页

--- src/api/getCarinfo.js
+++ src/api/getCarinfo.js
@@ -1,9 +1,9 @@
 import request from '../utils/request.js'
 
-	
-export function queryByCondition(data){
+//获取所有carinfo，有分页
+export function queryByPage(data){
 	return request({
-		url: '/carinfo/queryByCondition',//请求接口
+		url: '/carinfo/queryByPage',//请求接口
 		method: 'post',//请求方式
 		data
 	})
