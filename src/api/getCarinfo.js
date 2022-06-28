4f4561499dd2e64de33072513d4f15cb30c9672f
wlt <857582847@qq.com>
2022-06-28 10:39:21 +0800

    完成机电信息

--- src/api/getCarinfo.js
+++ src/api/getCarinfo.js
@@ -8,3 +8,26 @@ export function queryByPage(data){
 		data
 	})
 }
+//新增
+export function add(data){
+	return request({
+		url: '/carinfo/add',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+//修改
+export function update(data){
+	return request({
+		url: '/carinfo/update',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+export function deleteById(data){
+	return request({
+		url: '/carinfo/deleteById',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
