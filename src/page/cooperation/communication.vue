3f1ae86858ceda5ca314d25e12f8bc8c43472ed0
wangyaohui994 <1444988446@qq.com>
2022-06-29 10:37:51 +0800

    新增运输调度和对外合作

---
+++ src/page/cooperation/communication.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>信息交流</h1>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/page/transportationSchedule/carOperation.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>单车运营记录</h1>
+</template>
+
+<script>
+export default {
+  name: "carOperation"
+}
+</script>
+
+<style scoped>
+
+</style>
