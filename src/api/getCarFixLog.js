d53ef55058545eb36af085e6722a1e45a1120c54
dym <2876349876@qq.com>
2022-06-29 09:24:12 +0800

    汽车检修

--- src/api/getCarFixLog.js
+++ src/api/getCarFixLog.js
@@ -1,9 +1,30 @@
 import request from '../utils/request.js'
 
 	
-export function queryByCondition(data){
+export function queryByName(data){
 	return request({
-		url: '/addressinfo/queryByCondition',//请求接口
+		url: '/fixinfo/getByRepairmanName',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+export function update(data){
+	return request({
+		url: '/fixinfo/updateFixLog',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+export function insert(data){
+	return request({
+		url: '/fixinfo/insertFixLog',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+export function deleteFixLog(data){
+	return request({
+		url: '/fixinfo/deleteFixLog',//请求接口
 		method: 'post',//请求方式
 		data
 	})
