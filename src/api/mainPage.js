b1b49957a1b2dd44c7d9926280a9bdf0f871180b
qq1270735037 <qq1270735037@sina.com>
2022-07-01 08:42:42 +0800

    主页设计

---
+++ src/api/mainPage.js
@@ -0,0 +1,31 @@
+
+import request from '../utils/request.js'
+
+export function queryByCondition(data){
+    return request({
+        url: '/merchandiseinfo/queryByCondition',//请求接口
+        method: 'post',//请求方式
+        data
+    })
+}
+export function getByOperation(data){
+	return request({
+		url: '/carchange/getByOperation',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+export function selectByName(data){
+	return request({
+		url: '/matetialinfo/selectByName',//请求接口
+		method: 'post',//请求方式
+		data
+	})
+}
+export function queryByCondition1(data){
+    return request({
+        url: '/transportationinfo/queryByCondition',//请求接口
+        method: 'post',//请求方式
+        data
+    })
+}
